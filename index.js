ccd826decf7a65fca7a9bb7f7e635c2115218541
FahmidaMitu <fahmidamitu57@gmail.com>
2024-12-04 13:16:48 +0600

    Done

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
-function playgame(choice){
+//Function to choose button.
+function startGame(choice){
+  //define html game to display the functionality.
     const game = document.getElementById("game");
-
+//HTML, Bootstrap & JavaScript done together.
     if (choice === 'left') {
         game.innerHTML = `
-        <p>Swim or Wait?</p>
-        <button onclick="playgame('swim')">Swim</button>
-        <button onclick="playgame('wait')">Wait</button>
-        <button onclick="playgame('anything2')">Anything else</button>
-
+        <p class="fw-bold">Swim or Wait?</p>
+        <button onclick="startGame('swim')" type="button" class="btn btn-light">Swim</button>
+        <button onclick="startGame('wait')"type="button" class="btn btn-light">Wait</button>
+        <button onclick="startGame('anything2')"type="button" class="btn btn-light">Anything else</button>
         `;
       } else if (choice === 'right'){
         game.innerHTML = `
-        <p>Fall into a hole.</p>
-        <p>Game Over</p>
-        <button onclick="restartGame()">Restart</button>`;
+        <p class="fw-bold">Fall into a hole.</p>
+        <p class="fw-bold text-danger">Game Over</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`;
       }else if (choice === 'anything1'){
         game.innerHTML = `
-        <p>Fall into a hole.</p>
-        <p>Game Over</p>
-        <button onclick="restartGame()">Restart</button>`;  
+        <p class="fw-bold">Fall into a hole.</p>
+        <p class="fw-bold text-danger">Game Over</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`;  
       }else if(choice === 'swim'){
         game.innerHTML = `
-        <p>Attacked by trout</p>
-        <p>Game Over.</p>
-        <button onclick="restartGame()">Restart</button>`;  
+        <p class="fw-bold">Attacked by trout</p>
+        <p class="fw-bold text-danger">Game Over.</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`;  
       }else if(choice ==='anything2'){
         game.innerHTML =`
-        <p>Attacked by trout</p>
-        <p>Game Over.</p>
-        <button onclick="restartGame()">Restart</button>`;  
+        <p class="fw-bold">Attacked by trout</p>
+        <p class="fw-bold text-danger">Game Over.</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`;  
       }else if(choice === 'wait'){
         game.innerHTML =`
-        <p>Which door?</p>
-        <button onclick = "playgame('red')">Red</button>
-        <button onclick = "playgame('blue')">Blue</button>
-        <button onclick = "playgame('yellow')">Yellow</button>
-        <button onclick = "playgame('anything3')">Anything else</button>`;
+        <p class="fw-bold">Which door?</p>
+        <button onclick = "startGame('red')"type="button" class="btn btn-danger">Red</button>
+        <button onclick = "startGame('blue')"class="btn btn-primary">Blue</button>
+        <button onclick = "startGame('yellow')"type="button" class="btn btn-warning">Yellow</button>
+        <button onclick = "startGame('anything3')""type="button" class="btn btn-light">Anything else</button>`;
       }else if(choice === 'red'){
         game.innerHTML = `
-        <p>Burned by fire.</p>
-        <p>Game Over.</p>`;
+        <p class="fw-bold">Burned by fire.</p>
+        <p class="fw-bold text-danger">Game Over.</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`;
       }else if(choice === 'blue'){
         game.innerHTML = `
-        <p>Eaten by beasts</p>
-        <p>Game Over</p>`;
+        <p class="fw-bold">Eaten by beasts</p>
+        <p class="fw-bold text-danger">Game Over</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`;
       }else if( choice === 'yellow'){
         game.innerHTML = `
-        <p>You win!</p>`;
+        <p class="fw-bold text-success">You win!</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`;
       }else if (choice === 'anything3'){
         game.innerHTML =`
-        <p>Game Over</p>`;
+        <p class="fw-bold text-danger">Game Over</p>
+        <button onclick="restartGame()"type="button" class="btn btn-light">Restart</button>`; 
       }
     }
+    //To restart the game.
     function restartGame (){
       location.reload();
-
     }
-
+   
